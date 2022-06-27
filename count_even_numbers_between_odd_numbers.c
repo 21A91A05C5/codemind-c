288c3ae4b81a2b6db55f6ef2255cd6aaafffd0f6
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-06-27 22:48:24 +0530

    #include<stdio.h>  int main()  {      int n,arr[100],i,c=0,j;      scanf("%d",&n);      for(i=0;i<n;i++)      {          scanf("%d",&arr[i]);      }      for(i=1;i<n-1;i++)      {          c=0;          for(j=1;j<n-1;j++)          {              if(arr[j+1]%2 && arr[j-1]%2 && arr[j]%2==0)              {                  c++;              }          }      }      printf("%d",c);
     }

--- count_even_numbers_between_odd_numbers.c
+++ count_even_numbers_between_odd_numbers.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
-int main()
-{
-   int n,i,arr[100],j,c=0;
-   scanf("%d",&n);
-   for(i=0;i<n;i++)
-   {
-       scanf("%d",&arr[i]);
-   }
-   for(i=1;i<n-1;i++)
-   {
-       c=0;
-       for(j=1;j<n-1;j++)
-       {
-           if(arr[j+1]%2 && arr[j-1]%2 &&arr[j]%2==0 )
-           {
-               c++;
-           }
-       }
-    }
+ int main()
+ {
+     int n,arr[100],i,c=0,j;
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&arr[i]);
+     }
+     for(i=1;i<n-1;i++)
+     {
+         c=0;
+         for(j=1;j<n-1;j++)
+         {
+             if(arr[j+1]%2 && arr[j-1]%2 && arr[j]%2==0)
+             {
+                 c++;
+             }
+         }
+     }
      printf("%d",c);
-   }       
+    
+ }
