e03de6d73e1c82b7b832e872c39801a345b7b78c
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-06-27 22:25:56 +0530

    #include<stdio.h>  int main()  {      int n,arr[100],i,p,k,se=0;      scanf("%d",&n);      for(i=0;i<n;i++)      {          scanf("%d",&arr[i]);      }      scanf("%d",&k);      for(i=0;i<n;i++)      {          if(arr[i]==k)          {              se++;          }      }      printf("%d",se);  }

--- count_of_given_element_in_an_array.c
+++ count_of_given_element_in_an_array.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,j,arr[100],k,se=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    scanf("%d",&k);
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]==k)
-        {
-            se++;
-        }
-    }
-    printf("%d",se);
-}
+ int main()
+ {
+     int n,arr[100],i,p,k,se=0;
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&arr[i]);
+     }
+     scanf("%d",&k);
+     for(i=0;i<n;i++)
+     {
+         if(arr[i]==k)
+         {
+             se++;
+         }
+     }
+     printf("%d",se);
+ }
