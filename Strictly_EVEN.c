dd55b26c67f99943e89afee77bde5a7faa167360
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-06-29 22:02:38 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,sum=0,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             if(i%2!=0)             {                 printf("False");                 return 0;             }         }     }     printf("True"); }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],sum=0,k;
+    int n,arr[100],i,sum=0,k;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -20,5 +20,4 @@ int main()
         }
     }
     printf("True");
-    
-}    
+}
