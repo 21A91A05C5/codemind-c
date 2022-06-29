48dd659b406cebc92f6a2a46d78dfb3a4e52d2b4
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-06-29 21:11:31 +0530

    #include<stdio.h> int main() {     int n,arr[100],k,c=0,i;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k==0)         {             c++;         }     }     printf("%d",c); }

--- Count_elements_divisible_by_k.c
+++ Count_elements_divisible_by_k.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],k,c=0;
-    scanf("%d %d",&n,&k);
+    int n,arr[100],k,c=0,i;
+    scanf("%d%d",&n,&k);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
